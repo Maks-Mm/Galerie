756125b4141b7f6b8596fabdbed23f49a0ec77ef
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-12-19 01:32:37 +0100

    new Pakets

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ root.render(
 Resoursen (Time 06:23)
 
 https://www.youtube.com/watch?v=FiGmAI5e91M&list=PLillGF-RfqbY3c2r0htQyVbDJJoBFE6Rb&index=4
+
 In this project we will integrate Tailwind CSS with React and build an image gallery app with the Pixabay API
 
 https://pixabay.com/api/?key=44193362-398485989173fdc6e8e0d1dde&q=yellow+flowers&image_type=photo&pretty=true
