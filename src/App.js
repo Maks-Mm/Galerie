694ba47d710aa2c46ccc5f12129b481a464c08b0
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-12-23 21:49:07 +0100

    new App.js

--- src/App.js
+++ src/App.js
@@ -11,18 +11,24 @@ function App() {
 
 
 
+  // Version von Postman}https://pixabay.com/api/?key=44193362-398485989173fdc6e8e0d1dde&q=yellow+flowers&image_type=photo&pretty=true
   useEffect(() => {
-    // Version von Postman}https://pixabay.com/api/?key=44193362-398485989173fdc6e8e0d1dde&q=yellow+flowers&image_type=photo&pretty=true
-    fetch(
-      `https://pixabay.com/api/?key=44193362-398485989173fdc6e8e0d1dde&q=${term}&image_type=photo&pretty=true`
-    )
-    .then(res => res.json())
-    .then(data => {
-      setImages(data.hits);
-      setIsLoading(false);
-    })
-    .catch(err => console.log(err));
-}, [term]);
+    if (term) { // Überprüfen, ob ein Suchbegriff vorhanden ist
+      fetch(
+        `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+      )
+        .then(res => res.json())
+        .then(data => {
+          setImages(data.hits);
+          setIsLoading(false);
+        })
+        .catch(err => {
+          console.error(err);
+          setIsLoading(false); // Setzen Sie isLoading auf false bei einem Fehler
+        });
+    }
+  }, [term]);
+  
 
   return (
     <div className="container mx-auto">
