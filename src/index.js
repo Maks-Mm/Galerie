c824acdbbe8de94b5c6d6be03d7ea0803c710823
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2025-01-13 12:21:32 +0100

    ...

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-//import "./assets/tailwind.css";
+import "./assets/tailwind.css";
 import "./assets/main.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
