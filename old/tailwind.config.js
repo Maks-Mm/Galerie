739eb339aae79f55e3b94122e365b36bc37ba8ac
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2025-01-25 17:02:32 +0100

    old

---
+++ old/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path based on where your source files are located
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
